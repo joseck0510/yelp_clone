3bba3852e7d94ca7e784b0520e0a676d82db47a8
Robert Summers <rsummers02@gmail.com>
2016-08-23 21:58:23 +0100

    user can now add reviews to restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,6 +13,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:thougts, :rating)
+    params.require(:review).permit(:thoughts, :rating)
   end
 end
