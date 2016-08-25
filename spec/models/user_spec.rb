c5a1676195df79f651b74997cc87de8bd4ba86b7
Titus Iudean <iudeant@gmail.com>
2016-08-25 17:19:22 +0300

    Y3-Section2: begins user-reviews generation

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,7 @@
+
+describe User, type: :model do
+  user = User.create("ty@apps.com", "123456", "123456")
+  restaurant = Restaurant.new(name: "kf")
+  
+  it { is_expected.to have_many :reviewed_restaurants}
+end
